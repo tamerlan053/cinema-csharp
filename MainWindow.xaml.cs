3b527fa6420f9ae01ce98e6dabab6ffff632445b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-13 22:58:02 +0200

    Update MainWindow.xaml.cs
    Startcode added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Windows;
 
+namespace CinemaTicketApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        }
+    }
+}
